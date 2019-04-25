73b7a6a3f754924af2052ba6b557cfcdf44eb2c5
Amir P <apourrezaei@gmail.com>
2019-04-25 16:04:14 -0400

    GetOrder

--- bamazonCustomre.js
+++ bamazonCustomre.js
@@ -1,36 +1,52 @@
 require("dotenv").config();
 
 var mysql = require("mysql");
-
+var inquirer = require("inquirer");
 
 console.log(process.env.PW);
 
 var connection = mysql.createConnection({
     host: "localhost",
-    port:3306,
-    user:"root",
+    port: 3306,
+    user: "root",
 
-    password:process.env.PW,
-    database:"bamazon"
-})  
+    password: process.env.PW,
+    database: "bamazon"
+})
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    ListProducts();
-  });
-  
-  function ListProducts() {
+    // ListProducts();
+    GetOrder();
+    connection.end();
+});
+
+function ListProducts() {
     console.log("Querying prpoducts...\n");
     var query = connection.query(
-      "SELECT * FROM products",
-      function(err, res) {
-        console.log(res.affectedRows + " products returned!\n");
-        console.log("results: " + JSON.stringify(res, null, 4));
-      }
-    );
-  
-    // logs the actual query being run
-    console.log(query.sql);
-    connection.end();
-  }
+        "SELECT * FROM products",
+        function (err, res) {
+            console.log(res.affectedRows + " products returned!\n");
+            console.log("results: " + JSON.stringify(res, null, 4));
+            connection.end();
+        }
+    )
+}
+
+function GetOrder() {
+    inquirer.prompt([
+        {
+            name: "ID",
+            message: "Product Id"
+        },
+        {
+            name: "units",
+            message: "units"
+        }
+    ]).then(function (answers) {
+        console.log(answers);
+    }
+    )
+}
+
