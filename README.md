b1fc84f3684fdc748084eb283a8743a637bb4cb7
Amir P <apourrezaei@gmail.com>
2019-04-30 18:22:46 -0400

    Final Commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # amazonapp
+
+## Challenge 1:
+    
--- bamazonCustomre.js
+++ bamazonCustomre.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-console.log(process.env.PW);
+var commands = ["List Products", "Get Order"];
+
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -17,11 +18,28 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    // ListProducts();
-    GetOrder();
-    // console.log(order);
+    inquirer.prompt([
+        {
+            type: "rawlist",
+            name: "command",
+            choices: commands,
+            message: "Command"
+        }
+    ]).then(function (answer) {
+        switch (answer.command) {
+            case commands[0]:
+                ListProducts();
+                break;
+            case commands[1]:
+                GetOrder();
+                break;
+        }
+    });
 
-     // connection.end();
+
+
+
+    // connection.end();
 });
 
 function ListProducts() {
@@ -29,8 +47,7 @@ function ListProducts() {
     var query = connection.query(
         "SELECT * FROM products",
         function (err, res) {
-            console.log(res.affectedRows + " products returned!\n");
-            console.log("results: " + JSON.stringify(res, null, 4));
+            DisplayResults(res)
             connection.end();
         }
     )
@@ -54,13 +71,13 @@ function GetOrder() {
                 var stock_quantity = res[0].stock_quantity;
                 var ID = answers.ID;
                 var order_qty = answers.units;
-                console.log("results: " + stock_quantity);
-                if(answers.units > stock_quantity ) {
+                // console.log("results: " + stock_quantity);
+                if (answers.units > stock_quantity) {
                     console.log("Not enough units in stock");
 
                 }
                 else {
-                    updateUnits(ID, stock_quantity,order_qty);
+                    updateUnits(ID, stock_quantity, order_qty);
                 }
                 connection.end();
             }
@@ -70,14 +87,50 @@ function GetOrder() {
 }
 function updateUnits(id, qty, units) {
     var tempQty = qty;
-    tempQty -= units;   
+    tempQty -= units;
     var query = connection.query(
         "UPDATE products SET stock_quantity = " + tempQty + " where item_id=" + id,
         function (err, res) {
             console.log("Quantity Updated");
-           
-           // connection.end();
+
+            // connection.end();
         }
     )
 }
 
+function DisplayResults(table) {
+    CreateHeader(table);
+
+    DisplayData(table);
+
+}
+
+function DisplayData(table) {
+    var valuesStr = "";
+    for (var i in table) {
+        var temp = Object.values(table[i]);
+        for (var j = 0; j < temp.length; j++) {
+            valuesStr += temp[j] + "\t";
+        }
+        console.log(valuesStr);
+        valuesStr = "";
+    }
+}
+
+function CreateHeader(table) {
+    var keys = Object.keys(table[0]);
+    var headerStr = "";
+    var borderStr = "_";
+    for (var i in keys) {
+        headerStr += keys[i] + "\t";
+        var tempStr = keys[i] + "\t";
+        var len = tempStr.length;
+        for (var j = 0; j < len; j++) {
+            borderStr += "_";
+        }
+        borderStr += " ";
+    }
+    console.log(headerStr);
+    console.log(borderStr);
+    console.log("");
+}
